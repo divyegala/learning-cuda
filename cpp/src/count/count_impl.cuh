05072d748982ae760ff388f3696b45d901580593
divyegala <divyegala@gmail.com>
2020-09-20 20:35:26 -0700

    better style

--- cpp/src/count/count_impl.cuh
+++ cpp/src/count/count_impl.cuh
@@ -2,22 +2,22 @@
 
 namespace naive {
 
-    template <typename T, class F>
-    int count_if(T* arr, int size,
-                 F count_if_op) {
-        int *count_d;
-        int *count_h = new int[1];
-        cudaMalloc(&count_d, sizeof(int));
-        cudaMemset(count_d, 0, sizeof(int));
+template <typename T, class F>
+int count_if(T* arr, int size,
+                F count_if_op) {
+    int *count_d;
+    int *count_h = new int[1];
+    cudaMalloc(&count_d, sizeof(int));
+    cudaMemset(count_d, 0, sizeof(int));
+
+    int TPB = std::ceil((float) size / N_BLK);
     
-        int TPB = std::ceil((float) size / N_BLK);
-        
-        count_kernel<<<TPB, N_BLK>>> (arr, size, count_d, count_if_op);
-    
-        cudaMemcpy(count_h, count_d, sizeof(int), cudaMemcpyDeviceToHost);
-    
-        return *count_h;
-    }
-    
-    } // namespace naive
+    count_kernel<<<TPB, N_BLK>>> (arr, size, count_d, count_if_op);
+
+    cudaMemcpy(count_h, count_d, sizeof(int), cudaMemcpyDeviceToHost);
+
+    return *count_h;
+}
+
+} // namespace naive
     
