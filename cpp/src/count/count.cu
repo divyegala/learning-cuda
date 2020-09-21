c539713319b7aa0915154899e029209a650e2186
divyegala <divyegala@gmail.com>
2020-09-20 20:34:44 -0700

    rearranging files

--- cpp/src/count/count.cu
+++ cpp/src/count/count.cu
@@ -8,28 +8,8 @@
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 
-#include <learning_cuda/count/count_kernels.cuh>
+#include "count_impl.cuh"
 
-namespace naive {
-
-template <typename T, class F>
-int count_if(T* arr, int size,
-             F count_if_op) {
-    int *count_d;
-    int *count_h = new int[1];
-    cudaMalloc(&count_d, sizeof(int));
-    cudaMemset(count_d, 0, sizeof(int));
-
-    int TPB = std::ceil((float) size / N_BLK);
-    
-    count_kernel<<<TPB, N_BLK>>> (arr, size, count_d, count_if_op);
-
-    cudaMemcpy(count_h, count_d, sizeof(int), cudaMemcpyDeviceToHost);
-
-    return *count_h;
-}
-
-} // namespace naive
 
 int main() {
     int n_elems = 100000;
---
+++ cpp/src/count/count_impl.cuh
@@ -0,0 +1,23 @@
+#include <learning_cuda/count/count_kernels.cuh>
+
+namespace naive {
+
+    template <typename T, class F>
+    int count_if(T* arr, int size,
+                 F count_if_op) {
+        int *count_d;
+        int *count_h = new int[1];
+        cudaMalloc(&count_d, sizeof(int));
+        cudaMemset(count_d, 0, sizeof(int));
+    
+        int TPB = std::ceil((float) size / N_BLK);
+        
+        count_kernel<<<TPB, N_BLK>>> (arr, size, count_d, count_if_op);
+    
+        cudaMemcpy(count_h, count_d, sizeof(int), cudaMemcpyDeviceToHost);
+    
+        return *count_h;
+    }
+    
+    } // namespace naive
+    
